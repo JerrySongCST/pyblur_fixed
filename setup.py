92e3a40bf3cccc8961f2732e0cfdcf2c79eb664f
JerrySongCST <45647358+JerrySongCST@users.noreply.github.com>
2025-04-21 19:37:10 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setup(
     name="pyblur",
     version="0.1.0",
     packages=find_packages(),
-    install_requires=["opencv-python", "numpy"],
+    install_requires=["opencv-python", "numpy", "scipy"],
 )
